ddd107f93127e0d2b27a1fbee73321b43cf4e60e
vanya992 <vanjacrv92@gmail.com>
2023-02-20 20:37:45 +0100

    Index page finished

--- js/index.js
+++ js/index.js
@@ -1,36 +1,58 @@
+const detailsContainer = document.querySelector(".container");
+
+const messageContainer = document.querySelector(".message");
+
+const queryString = document.location.search;
+
+const params = new URLSearchParams(queryString);
+
+const id = params.get("id");
+
+console.log(id);
 
 const url = "https://hp-api.onrender.com/api/characters";
 
-const detailsContainer = document.querySelector(".container")
-
 async function fetchCharacters() {
-        const response = await fetch(url);
+    try {const response = await fetch(url);
 
         const results = await response.json();
 
-       // console.log(results);
+        console.log(results);
 
-    const characters = results;
+        const characters = results;
 
-    detailsContainer.innerHTML = "";
+        detailsContainer.innerHTML = "";
 
-    for (let i = 0; i < characters.length; i++) {
-        console.log(characters[i].name);
-   
-    if (i === 20) {
-        break;
-    }
-
-    detailsContainer.innerHTML += `<div class="container">${characters[i].name}</div>`
+           for (let i = 0; i < results.length; i++) {
+        
+                if (!results[i].image || !results[i].name || !results[i].house) {
+                                continue;
+                            }
+                    
+                detailsContainer.innerHTML += `<a href="details.html?id=${results[i].id}" class="cards">
+                                                <div><h2>${results[i].name}</h2></div>
+                                                <div><img class="img" src="${results[i].image}" alt="${results[i].name}"></div>
+                                                <div><p>${results[i].house}</p></div>
+                                                </a>`
+           }
+        
     } 
+    
+       catch (error) {
+            console.log(error);
+           messageContainer.innerHTML = error;
+    }
 
 }
 
 fetchCharacters();
 
 
-// const loader = document.getElementById("loader")
+const loader = document.getElementById("loader")
 
-// window.addEventListener("load", function () {
-//     loader.style.display = "none";
-// })
+window.addEventListener("load", function () {
+    loader.style.display = "none";
+})
+
+
+
